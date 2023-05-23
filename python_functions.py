70643f03a40f8711fac4054fb126825975211bef
Nicholas Manning <njmanning212@gmail.com>
2023-05-23 09:19:31 -0500

    solve challenge 2

--- python_functions.py
+++ python_functions.py
@@ -10,5 +10,23 @@ def sum_to(n):
         sum = sum+i
     return sum
 
-print(sum_to(6))
-print(sum_to(10))
+# print(sum_to(6))
+# print(sum_to(10))
+
+# ----------------------------------------------------------------
+
+#Challenge 2:
+
+# Write a function named largest that takes a list of numbers as an argument and returns the largest number in that list.
+
+#Solution:
+
+def largest(list):
+    largest = list[0]
+    for i in list:
+        if i > largest:
+            largest = i
+    return largest
+
+print (largest([1, 2, 3, 4, 0]))
+print (largest([10, 4, 2, 231, 91, 54]))
